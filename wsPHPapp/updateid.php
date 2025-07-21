ce6fa8b65372f401e677f165025fd4296ac47b6e
lorped <lorped@gmail.com>
2025-07-21 15:54:45 +0200

    1.00.06

--- wsPHPapp/updateid.php
+++ wsPHPapp/updateid.php
@@ -1,12 +1,11 @@
-<?
-header("Access-Control-Allow-Origin: *");
+<?php
 
 //http://stackoverflow.com/questions/18382740/cors-not-working-php
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-	header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-	header('Access-Control-Allow-Credentials: true');
-   header('Access-Control-Max-Age: 86400');    // cache for 1 day
- }
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
 
 // Access-Control headers are received during OPTIONS requests
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
